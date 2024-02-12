e912b68597fd45a93b5fedbf06bdcafc517beb39
CasttCK <vini.kronemberger@gmail.com>
2024-02-12 17:40:49 -0300

    Feat: Criação classe configuration

---
+++ src/main/java/br/com/fasterfood/fasterapi/Configuration/FasterFoodConfig.java
@@ -0,0 +1,16 @@
+package br.com.fasterfood.fasterapi.Configuration;
+
+import br.com.fasterfood.fasterapi.Notificacao.NotificadorEmail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FasterFoodConfig {
+    @Bean
+    public NotificadorEmail notificadorEmail (){
+        NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
+        notificador.setCaixaAlta(true);
+
+        return notificador;
+    }
+}
--- src/main/java/br/com/fasterfood/fasterapi/Notificacao/NotificadorEmail.java
+++ src/main/java/br/com/fasterfood/fasterapi/Notificacao/NotificadorEmail.java
@@ -1,17 +1,28 @@
 package br.com.fasterfood.fasterapi.Notificacao;
 
 import br.com.fasterfood.fasterapi.Model.Cliente;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NotificadorEmail implements Notificador {
+    private String hostSmtp;
+    private boolean caixaAlta;
+
+    public NotificadorEmail(String hostSmtp) {
+        this.hostSmtp = hostSmtp;
+    }
+
+    public void setCaixaAlta(boolean caixaAlta) {
+        this.caixaAlta = caixaAlta;
+    }
 
     @Override
     public String notificarAtivacao(Cliente cliente, String mensagem){
 //        TODO
 //        fazer aqui a lógica para envio de email. Estudar mais sobre as bibliotecas
 //        e fazr o serviço.
-        System.out.printf("Notificando o usuário %s, através do e-mail %s",
+        if(caixaAlta){
+            mensagem = mensagem.toUpperCase();
+        }
+        System.out.printf("Notificando o usuário %s, através do e-mail %s | Mensagem: %s",
                 cliente.getNome(), cliente.getEmail(), mensagem);
 
         return "Ativado!";
@@ -19,7 +30,11 @@ public class NotificadorEmail implements Notificador {
 
     @Override
     public String notificarDesativacao(Cliente cliente, String mensagem){
-        System.out.printf("Notificando o usuário %s, através do e-mail %s",
+        if(caixaAlta){
+            mensagem = mensagem.toUpperCase();
+        }
+
+        System.out.printf("Notificando o usuário %s, através do e-mail %s | Mensagem: %s",
                 cliente.getNome(), cliente.getEmail(), mensagem);
 
         return "Desativado!";
