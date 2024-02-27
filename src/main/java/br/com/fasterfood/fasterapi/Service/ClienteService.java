ae5483c898993948952d49bdfa3d09788889af97
CasttCK <vini.kronemberger@gmail.com>
2024-02-27 19:23:40 -0300

    Feat: Utilização de init e destroy no bean

--- src/main/java/br/com/fasterfood/fasterapi/Service/ClienteService.java
+++ src/main/java/br/com/fasterfood/fasterapi/Service/ClienteService.java
@@ -4,6 +4,8 @@ import br.com.fasterfood.fasterapi.Model.Cliente;
 import br.com.fasterfood.fasterapi.Notificacao.NivelUrgencia;
 import br.com.fasterfood.fasterapi.Notificacao.Notificador;
 import br.com.fasterfood.fasterapi.Notificacao.TipoDoNotificador;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,16 @@ public class ClienteService {
     @TipoDoNotificador(NivelUrgencia.BAIXA)
     @Autowired
     private Notificador notificador;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("INIT");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
 
     public void ativarCliente(Cliente cliente){
         cliente.ativar();
